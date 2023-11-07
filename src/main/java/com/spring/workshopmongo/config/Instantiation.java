775e0a9539715b2752913e20e9974e23749b7c1b
Fellipe <fehb21@gmail.com>
2023-11-07 20:25:04 -0300

    Utilizando o padrao DTO para retornar usuarios

---
+++ src/main/java/com/spring/workshopmongo/config/Instantiation.java
@@ -0,0 +1,21 @@
+package com.spring.workshopmongo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.spring.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+	}
+
+}
